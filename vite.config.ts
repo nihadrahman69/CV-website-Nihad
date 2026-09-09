d1e4dc5f0d1bf2814cb0ae2a3c4f89027eb04a0e
nihadrahman69 <rahmannih69@gmail.com>
2026-09-10 05:20:10 +0600

    Added github pages workflow and update base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/portfolio-app/', 
   plugins: [tailwindcss(), react()],
 })
